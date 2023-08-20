7d9dafab00dc1cfc0a6deb1463d307f46074cc16
yuzo5210 <yuzo5210@colorado.edu>
2023-08-20 19:18:09 +0800

    first update

---
+++ Src/Client/Assets/Scripts/LodingManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+using SkillBridge.Message;
+using ProtoBuf;
+
+public class LodingManager : MonoBehaviour {
+	public Slider loadingSlider;
+	public Text loadingText;
+	public Text loadingAmount;
+
+	private string sceneName = "LoginAndRegister";
+	IEnumerator Start()
+	{
+		log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo("log4net.xml"));
+		UnityLogger.Init();
+		Common.Log.Init("Unity");
+		Common.Log.Info("LoadingManager start");
+
+		for (float i = 10; i < 100;)
+		{
+			i += Random.Range(0.1f, 1.5f);
+			loadingSlider.value = i;
+			loadingAmount.text = (int)i + "%";
+			yield return new WaitForEndOfFrame();
+		}
+		yield return null;
+
+		Application.LoadLevel(sceneName);
+	} 
+
+}
---
+++ Src/Client/Assets/Scripts/UI/UILogin.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Services;
+using SkillBridge.Message;
+
+public class UILogin : MonoBehaviour
+{
+
+    public InputField username;
+    public InputField password;
+    public Toggle agreement;
+    public Toggle rememberPassword;
+    public Button buttonLogin;
+
+    void Start()
+    {
+        UserService.Instance.OnLogin = this.OnLogin;
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    public void OnClickLogin()
+    {
+        if (string.IsNullOrEmpty(this.username.text))
+        {
+            MessageBox.Show("请输入账号");
+            return;
+        }
+        if (string.IsNullOrEmpty(this.password.text))
+        {
+            MessageBox.Show("请输入密码");
+            return;
+        }
+        if (!agreement.isOn)
+        {
+            MessageBox.Show("请阅读并同意相关协议");
+            return;
+        }
+        UserService.Instance.SendLogin(this.username.text, this.password.text);
+    }
+
+    void OnLogin(SkillBridge.Message.Result result, string msg)
+    {
+        if (result == Result.Success)
+        {
+            //登录成功，进入角色选择
+            MessageBox.Show("登录成功，准备角色选择" + msg, "提示", MessageBoxType.Information);
+            SceneManager.Instance.LoadScene("CharSelect");
+        }
+        else
+        {
+            MessageBox.Show(msg, "错误", MessageBoxType.Error);
+        }
+    }
+
+}
